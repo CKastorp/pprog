f723af3dc35cc537f67047e456cdb990c8d8c369
CKastorp <36626889+CKastorp@users.noreply.github.com>
2018-02-19 13:40:37 +0100

    Add files via upload

--- main.c
+++ main.c
@@ -1,12 +1,18 @@
+#include "komplex.h"
 #include"stdio.h"
-void epsilon(void);
-void float_sums(void);
-void maxima (void);
-int main(){
-maxima();
-epsilon();
-float_sums();
+#include"math.h"
+#define TINY 1e-6
 
+int main(){
+komplex a={1,2};
+komplex b={3,5};
+komplex R={4,7};
+printf("Testing komplex_add\n");
+komplex r=komplex_add(a,b);
+komplex_print("a=",a);
+komplex_print("b=",b);
+komplex_print("a+b should be", R);
+komplex_print("a+b really is",r);
 
 return(0);
 }
